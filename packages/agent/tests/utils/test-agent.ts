9a76b46b1ad112b9ea3d1dd67e9dcb80812f3fa2
Liran Cohen <c.liran.c@gmail.com>
2023-09-12 19:17:56 -0400

    bring parts of sync up outside of push/pull

--- packages/agent/tests/utils/test-agent.ts
+++ packages/agent/tests/utils/test-agent.ts
@@ -205,3 +205,7 @@ export class TestAgent implements Web5ManagedAgent {
     throw new Error('Not implemented');
   }
 }
+
+export async function sleep(duration: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
